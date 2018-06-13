401bb31cec9b55ab8a190709f2ae75e9da1ffb4d
Si Kai <sikai.ngsk@gmail.com>
2018-06-13 14:41:03 +0800

    filtered out unwanted columns

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ import csv
 # Change this according to desired scope
 scope = 'AnnouncementLast12Months'
 # Change this according to maximum number of results desired
-limit = '100'
+limit = '100000'
 url = 'http://www.sgx.com/proxy/SgxDominoHttpProxy?timeout=100&dominoHost=http%3A%2F%2Finfofeed.sgx.com%2FApps%3FA%3DCOW_CorpAnnouncement_Content%26B%3D{0}%26R_C%3D%26C_T%3D{1}'.format(scope, limit)
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
@@ -18,12 +18,14 @@ cleaned_result = result.content[4:]
 result_obj = json.loads(cleaned_result)
 # do we need to know about number of shares?
 # num_of_shares = result_obj['SHARES']
+
 items = result_obj['items']
 
 # Writing results to csv file
 with open('result.csv', 'wb') as csvfile:
-    fieldnames = ['key', 'Date', 'Time', 'IssuerName', 'SecurityName', 'GroupCode', 'CategoryCode', 'CategoryName', 'AnnTitle', 'BroadcastDateTime', 'Siblings']
+    fieldnames = ['key', 'Date', 'Time', 'IssuerName', 'SecurityName', 'GroupCode',  'CategoryName', 'AnnTitle']
     my_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     my_writer.writeheader()
     for item in items:
-        my_writer.writerow(item)
+        filtered_item = {k: item.get(k) for k in tuple(fieldnames)}
+        my_writer.writerow(filtered_item)
